760f9d7a7e02f181a1c20e8d6f07202c0dfd7829
Lucas Forte <lucasforte92@gmail.com>
2021-05-22 10:00:47 -0300

    I've done some adjustments on box-shadows, added react-icons lib and finished presentation component

--- src/components/Presentation/index.tsx
+++ src/components/Presentation/index.tsx
@@ -3,8 +3,23 @@ import "./styles.scss";
 const Presentation = () => {
   return (
     <main className="page-presentation">
-      <h1></h1>
-      <p></p>
+      <h1>Lorem Ipsum sit dat met agader lirium</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+        sagittis magna placerat ipsum suscipit, quis tincidunt nibh tempor. Ut
+        tempor semper nunc non congue. Quisque purus erat, auctor non sem in,
+        maximus euismod nisl. Vestibulum id commodo metus, sed viverra dolor. Ut
+        at vehicula nulla, eu lacinia est. Lorem ipsum dolor sit amet,
+        consectetur adipiscing elit. Aliquam sagittis magna placerat ipsum
+        suscipit, quis tincidunt nibh tempor. Ut tempor semper nunc non congue.
+        Quisque purus erat, auctor non sem in, maximus euismod nisl. Vestibulum
+        id commodo metus, sed viverra dolor. Ut at vehicula nulla, eu lacinia
+        est.
+      </p>
+      <div className="page-presentation__partner">
+        <span>Parceiro</span>
+        <div className="partner__logo">LOGO</div>
+      </div>
     </main>
   );
 };
