d90e2ba77c604ae7b8fe22d6d69d6e87bae0364a
Lucas Forte <lucasforte92@gmail.com>
2021-05-22 12:41:32 -0300

    Finished the services section

--- src/components/Services/index.tsx
+++ src/components/Services/index.tsx
@@ -1,5 +1,51 @@
+import "./styles.scss";
+
+import {
+  AiOutlineMedicineBox,
+  GiMedicines,
+  RiHotelBedLine,
+  FaUserNurse,
+  FaHeartbeat,
+  FaStethoscope,
+} from "react-icons/all";
+
 const Services = () => {
-  return <div></div>;
+  return (
+    <div className="page-services">
+      <h2>Alguns dos serviços que são realizados no atendimento domiciliar</h2>
+      <div className="services-grid">
+        <div className="services-grid__item">
+          <FaStethoscope />
+          <p>Consulta Médica</p>
+        </div>
+        <div className="services-grid__item">
+          <FaUserNurse />
+          <p>Aconselhamento em Enfermagem</p>
+        </div>
+        <div className="services-grid__item">
+          <AiOutlineMedicineBox />
+          <p>Troca de Curativos</p>
+        </div>
+        <div className="services-grid__item">
+          <RiHotelBedLine />
+          <p>Troca de Sondas</p>
+        </div>
+        <div className="services-grid__item">
+          <GiMedicines />
+          <p>Administração de Medicamentos</p>
+        </div>
+        <div className="services-grid__item">
+          <FaHeartbeat />
+          <p>Eletrocardiograma com Laudo</p>
+        </div>
+      </div>
+      <div>
+        <button type="button" className="btn btn--confirm">
+          Ver mais serviços
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Services;
